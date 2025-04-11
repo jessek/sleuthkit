9aca7dc6cef5a5be3b8a6ebfe87f759798693961
Simson Garfinkel <simsong@acm.org>
2025-04-11 08:11:06 -0400

    patch for mingw

--- test/tsk/img/test_img.h
+++ test/tsk/img/test_img.h
@@ -1,13 +1,17 @@
 #ifndef TEST_IMG_H
 #define TEST_IMG_H
 
-#include "tsk/base/tsk_os_cpp.h"        // needed for TSK_TSTRING
+#include "tsk/base/tsk_os_cpp.h"  // for TSK_TCHAR, TSK_TSTRING
+#include <cstdlib>                // for getenv / _wgetenv
+#include <stdexcept>             // for std::runtime_error
+#include <string>
+#include <algorithm>             // for std::replace
 
-
+// Returns $srcdir/test/data/<relative_path>
 #ifdef TSK_WIN32_WIDE
 inline std::wstring prepend_test_data_dir(const wchar_t* relative_path) {
     const wchar_t* base = _wgetenv(L"srcdir");
-    if (!base) throw std::runtime_error("srcdir");
+    if (!base) throw std::runtime_error("srcdir not set");
     std::wstring full = base;
     full += L"/test/data/";
     full += relative_path;
@@ -16,7 +20,7 @@ inline std::wstring prepend_test_data_dir(const wchar_t* relative_path) {
 #else
 inline std::string prepend_test_data_dir(const char* relative_path) {
     const char* base = getenv("srcdir");
-    if (!base) throw std::runtime_error("srcdir");
+    if (!base) throw std::runtime_error("srcdir not set");
     std::string full = base;
     full += "/test/data/";
     full += relative_path;
@@ -31,4 +35,5 @@ inline void fix_slashes_for_windows(TSK_TSTRING &path) {
 #else
 inline void fix_slashes_for_windows(TSK_TSTRING &) {}
 #endif
-#endif
+
+#endif  // TEST_IMG_H
