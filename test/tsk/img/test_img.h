ee05603dc50867a2f00739d512227bf39e2d35dd
Simson Garfinkel <simsong@acm.org>
2025-04-14 11:11:34 -0400

    simplify

--- test/tsk/img/test_img.h
+++ test/tsk/img/test_img.h
@@ -7,35 +7,22 @@
 #include <string>
 #include <algorithm>
 
-// Generic template for char/wchar_t
-template <typename CharT>
-inline std::basic_string<CharT> prepend_test_data_dir(const CharT* relative_path);
-
-// Specialization for char
-template <>
-inline std::string prepend_test_data_dir<char>(const char* relative_path) {
-    const char* base = getenv("srcdir");
-    if (!base) throw std::runtime_error("srcdir not set");
-    std::string full = base;
-    full += "/test/data/";
-    full += relative_path;
-    return full;
-}
-
-// Specialization for wchar_t
-template <>
-inline std::wstring prepend_test_data_dir<wchar_t>(const wchar_t* relative_path) {
+// Convenience wrapper for current build type
+inline std::basic_string<TSK_TCHAR> prepend_test_data_dir(const TSK_TCHAR* relative_path) {
+#ifdef TSK_WIN32
     const wchar_t* base = _wgetenv(L"srcdir");
     if (!base) throw std::runtime_error("srcdir not set");
     std::wstring full = base;
     full += L"/test/data/";
     full += relative_path;
+#else
+    const char* base = getenv("srcdir");
+    if (!base) throw std::runtime_error("srcdir not set");
+    std::string full = base;
+    full += "/test/data/";
+    full += relative_path;
+#endif
     return full;
-}
-
-// Convenience wrapper for current build type
-inline std::basic_string<TSK_TCHAR> prepend_test_data_dir(const TSK_TCHAR* relative_path) {
-    return prepend_test_data_dir<TSK_TCHAR>(relative_path);
 }
 
 // Slash-fixing for Windows
