88e7f043fefd5c9abe87cc54f2538212bdd11b7b
Simson Garfinkel <simsong@acm.org>
2025-04-14 08:29:19 -0400

    try a templated function

--- test/tsk/img/test_img.h
+++ test/tsk/img/test_img.h
@@ -3,22 +3,17 @@
 
 #include "tsk/base/tsk_os_cpp.h"  // for TSK_TCHAR, TSK_TSTRING
 #include <cstdlib>                // for getenv / _wgetenv
-#include <stdexcept>             // for std::runtime_error
+#include <stdexcept>
 #include <string>
-#include <algorithm>             // for std::replace
+#include <algorithm>
 
-// Internal versions
-#ifdef TSK_WIN32_WIDE
-inline std::wstring prepend_test_data_dir_w(const wchar_t* relative_path) {
-    const wchar_t* base = _wgetenv(L"srcdir");
-    if (!base) throw std::runtime_error("srcdir not set");
-    std::wstring full = base;
-    full += L"/test/data/";
-    full += relative_path;
-    return full;
-}
-#else
-inline std::string prepend_test_data_dir_a(const char* relative_path) {
+// Generic template for char/wchar_t
+template <typename CharT>
+inline std::basic_string<CharT> prepend_test_data_dir(const CharT* relative_path);
+
+// Specialization for char
+template <>
+inline std::string prepend_test_data_dir<char>(const char* relative_path) {
     const char* base = getenv("srcdir");
     if (!base) throw std::runtime_error("srcdir not set");
     std::string full = base;
@@ -26,18 +21,24 @@ inline std::string prepend_test_data_dir_a(const char* relative_path) {
     full += relative_path;
     return full;
 }
-#endif
 
-// Unified TSK_TCHAR version
-inline std::basic_string<TSK_TCHAR> prepend_test_data_dir(const TSK_TCHAR* relative_path) {
-#ifdef TSK_WIN32_WIDE
-    return prepend_test_data_dir_w(relative_path);
-#else
-    return prepend_test_data_dir_a(relative_path);
-#endif
+// Specialization for wchar_t
+template <>
+inline std::wstring prepend_test_data_dir<wchar_t>(const wchar_t* relative_path) {
+    const wchar_t* base = _wgetenv(L"srcdir");
+    if (!base) throw std::runtime_error("srcdir not set");
+    std::wstring full = base;
+    full += L"/test/data/";
+    full += relative_path;
+    return full;
 }
 
-// Only needed for Windows
+// Convenience wrapper for current build type
+inline std::basic_string<TSK_TCHAR> prepend_test_data_dir(const TSK_TCHAR* relative_path) {
+    return prepend_test_data_dir<TSK_TCHAR>(relative_path);
+}
+
+// Slash-fixing for Windows
 #ifdef TSK_WIN32
 inline void fix_slashes_for_windows(TSK_TSTRING &path) {
     std::replace(path.begin(), path.end(), _TSK_T('/'), _TSK_T('\\'));
